b43c5ad56ce570c299b6186b7ea8e82526d0a4e3
0xMillz <37815163+0xMillz@users.noreply.github.com>
2024-12-12 06:45:36 -0600

    make year dynamic

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("Welcome to MILLZDOTETH LLC!");
+// Set the current year dynamically in the footer
+const yearElement = document.getElementById('currentYear');
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear.toString();
